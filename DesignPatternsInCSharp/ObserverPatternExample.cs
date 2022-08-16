b00cad738d0d844b91642dea0586d856425b34e3
Abhilash <abhilashgraja@gmail.com>
2022-08-16 12:29:22 +0530

    observer using events

--- DesignPatternsInCSharp/ObserverPatternExample.cs
+++ DesignPatternsInCSharp/ObserverPatternExample.cs
@@ -7,6 +7,15 @@
 			var weatherProvider = new Publisher();
 			var currentConditionsDisplay = new CurrentConditionsDisplay(weatherProvider);
 			weatherProvider.SetMeasurements(34, 80, 90);
+
+			var weatherDataProvider = new WeatherDataProvider();
+			weatherDataProvider.RaiseWeatherDataChangedEvent += WeatherDataProvider_RaiseWeatherDataChangedEvent;
+			weatherDataProvider.NotifyDisplays(35, 81, 91);
+		}
+
+		private void WeatherDataProvider_RaiseWeatherDataChangedEvent(object sender, WeatherDataEventArgs e)
+		{
+			Console.WriteLine("Current Conditions : Temp = {0} Deg | Humidity = {1}% | Pressure = {2} bar", e.data.Temperature, e.data.Humidity, e.data.Pressure);
 		}
 	}
 
@@ -83,6 +92,51 @@
 		{
 			this.data = data;
 			Display();
+		}
+	}
+
+	public class WeatherDataEventArgs : EventArgs
+	{
+		public WeatherData data { get; private set; }
+		public WeatherDataEventArgs(WeatherData data)
+		{
+			this.data = data;
+		}
+	}
+
+	public class WeatherDataProvider : IDisposable
+	{
+		public event EventHandler<WeatherDataEventArgs> RaiseWeatherDataChangedEvent;
+
+		protected virtual void OnRaiseWeatherDataChangedEvent(WeatherDataEventArgs e)
+		{
+			// Make a temporary copy of the event to avoid possibility of
+			// a race condition if the last subscriber unsubscribes
+			// immediately after the null check and before the event is raised.
+			EventHandler<WeatherDataEventArgs> handler = RaiseWeatherDataChangedEvent;
+
+			if (handler != null)
+			{
+				handler(this, e);
+			}
+		}
+
+		public void NotifyDisplays(float temp, float humid, float pres)
+		{
+			OnRaiseWeatherDataChangedEvent
+				 (new WeatherDataEventArgs(new WeatherData(temp, humid, pres)));
+		}
+
+		public void Dispose()
+		{
+			if (RaiseWeatherDataChangedEvent != null)
+			{
+				foreach (EventHandler<WeatherDataEventArgs>
+				item in RaiseWeatherDataChangedEvent.GetInvocationList())
+				{
+					RaiseWeatherDataChangedEvent -= item;
+				}
+			}
 		}
 	}
 }
